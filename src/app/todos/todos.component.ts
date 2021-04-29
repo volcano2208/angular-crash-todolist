7735aeac62db82dce308923e3ca789f784201405
Thanh Pham Tien <ptthanh5@cmc.com.vn>
2021-04-29 11:33:47 +0700

    complete-task-tracker

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../models/Todo';
+import { TodoService } from '../services/todo.service';
+
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    //Remove in UI
+    this.todos = this.todos.filter(todo => todo.id !== todo.id);
+    //Remove in Server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
+  }
+
+}
